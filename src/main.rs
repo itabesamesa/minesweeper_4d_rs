d6d9fbd2c151f3ab6585216f513346924b0199a4
itabesamesa <laura418@proton.me>
2025-11-24 00:43:05 +0100

    movement and display stuff i think

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,28 @@ use ratatui::{
 };
 use mersenne_twister::*;
 use rand::{Rng, SeedableRng, rngs::ThreadRng};
+use std::process;
+
+enum MinesweeperChar {
+    Bomb,
+    Flag,
+}
+
+impl MinesweeperChar {
+    fn as_str(&self) -> &'static str {
+        match self {
+            MinesweeperChar::Bomb => "💣",
+            MinesweeperChar::Flag => "🏴"
+        }
+    }
+}
 
 #[derive(Copy, Clone, Debug, Default)]
 struct Point {
-    x: usize,
-    y: usize,
-    z: usize,
-    w: usize
+    x: i16,
+    y: i16,
+    z: i16,
+    w: i16
 }
 
 impl PartialEq for Point {
@@ -28,20 +43,29 @@ impl PartialEq for Point {
     }
 }
 
-impl PartialOrd for Point {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        if self == other {
-            Some(Ordering::Equal)
+impl Eq for Point {}
+
+impl Ord for Point {
+    fn cmp(&self, other: &Self) -> Ordering { //ordering is very iffy cause obviously... best just
+                                              //work around and don't touch!
+        if self.eq(&other) {
+            Ordering::Equal
         } else if self.x < other.x && self.y < other.y && self.z < other.z && self.w < other.w {
-            Some(Ordering::Less)
+            Ordering::Less
         } else {
-            Some(Ordering::Greater)
+            Ordering::Greater
         }
     }
 }
 
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+
 impl Point {
-    fn random_range(&mut self, mut rng: ThreadRng, max: Point) {
+    fn random_range(&mut self, rng: &mut ThreadRng, max: Point) {
         self.x = rng.random_range(self.x..max.x);
         self.y = rng.random_range(self.y..max.y);
         self.z = rng.random_range(self.z..max.z);
@@ -49,25 +73,77 @@ impl Point {
     }
 
     fn to_1d(self, dim: Point) -> usize {
-        ((self.w * dim.z + self.z) * dim.y + self.y) * dim.x + self.x
+        (((self.w * dim.z + self.z) * dim.y + self.y) * dim.x + self.x).try_into().unwrap()
     }
 
     fn calc_area(self) -> usize {
-        return self.x*self.y*self.z*self.w;
+        return (self.x*self.y*self.z*self.w).try_into().unwrap();
+    }
+
+    fn offset(self, p: Point) -> Point {
+        Point {
+            x: self.x+p.x,
+            y: self.y+p.y,
+            z: self.z+p.z,
+            w: self.w+p.w
+        }
     }
 }
 
 #[derive(Copy, Clone, Debug)]
 struct MinesweeperCell {
+    coord: Point,
     is_bomb: bool,
     state: u8,
     abs: u8,
-    rel: i8
+    rel: i8,
+    is_active: bool,
+    in_active_neighbourhood: bool
 }
 
 impl Widget for MinesweeperCell {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        buf.set_string(area.left(), area.top(), format!("{:2}", self.abs), Style::default().fg(Color::Green));
+        //let bg = if (self.coord.x + self.coord.y)%2 == 0 {Color::Rgb(0xc6, 0xc6, 0xc6)} else {Color::Rgb(0xb8, 0xb8, 0xb8)}; //Light
+        let bg = if self.is_active {
+            Color::Rgb(255, 42, 255)
+        } else if self.in_active_neighbourhood {
+            if (self.coord.x + self.coord.y)%2 == 0 {Color::Rgb(128, 115, 128)} else {Color::Rgb(89, 80, 89)} //Covered
+        } else {
+            if (self.coord.x + self.coord.y)%2 == 0 {Color::Rgb(0x66, 0x66, 0x66)} else {Color::Rgb(0x3b, 0x3b, 0x3b)} //Covered
+        };
+        if self.is_bomb {
+            buf.set_string(area.left(), area.top(), MinesweeperChar::Bomb.as_str(), Style::default().bg(bg));
+        } else if self.abs > 0 {
+            buf.set_string(area.left(), area.top(), format!("{:2}", self.abs), Style::default().fg(Color::White).bg(bg));
+        } else {
+            buf.set_string(area.left(), area.top(), "  ", Style::default().bg(bg));
+        }
+    }
+}
+
+impl MinesweeperCell {
+    fn set_bomb(&mut self, t: bool) {
+        self.is_bomb = t;
+    }
+
+    fn set_coord(&mut self, p: Point) {
+        self.coord.x = p.x;
+        self.coord.y = p.y;
+        self.coord.z = p.z;
+        self.coord.w = p.w;
+    }
+
+    fn set_active(&mut self, t: bool) {
+        self.is_active = t;
+    }
+
+    fn set_active_neighbourhood(&mut self, t: bool) {
+        self.in_active_neighbourhood = t;
+    }
+
+    fn inc_all(&mut self) {
+        self.abs += 1;
+        self.rel += 1;
     }
 }
 
@@ -95,8 +171,8 @@ impl Widget for MinesweeperField {
         let grids = grid_rows.iter().flat_map(|&grid_row| z_horizontal.split(grid_row).to_vec());
 
         for (i, grid) in grids.enumerate() {
-            let w = i/self.dim.w;
-            let z = i%self.dim.z;
+            let w = (i as i16)/self.dim.w;
+            let z = (i as i16)%self.dim.z;
             let x_constraints = (0..self.dim.x).map(|_| Constraint::Length(2));
             let y_constraints = (0..self.dim.y).map(|_| Constraint::Length(1));
             let x_horizontal = Layout::horizontal(x_constraints).spacing(0);
@@ -105,8 +181,8 @@ impl Widget for MinesweeperField {
             let rows = y_vertical.split(grid);
             let cells = rows.iter().flat_map(|&row| x_horizontal.split(row).to_vec());
             for (j, cell) in cells.enumerate() {
-                let y = j/self.dim.y;
-                let x = j%self.dim.x;
+                let y = (j as i16)/self.dim.y;
+                let x = (j as i16)%self.dim.x;
                 self.field[Point {x: x, y: y, z: z, w: w}.to_1d(self.dim)].render(cell, buf)
             }
         }
@@ -125,29 +201,137 @@ impl MinesweeperField {
         self.area = self.dim.calc_area();
         self.field = vec![
             MinesweeperCell {
+                coord: Point {x: 0, y: 0, z: 0, w: 0},
                 is_bomb: false,
                 state: 0,
                 abs: 0,
-                rel: 0
-            }; self.area + 1
+                rel: 0,
+                is_active: false,
+                in_active_neighbourhood: false
+            }; self.area
         ];
+        for w in 0..self.dim.w {
+            for z in 0..self.dim.z {
+                for y in 0..self.dim.y {
+                    for x in 0..self.dim.x {
+                        let coord = Point {x: x, y: y, z: z, w: w};
+                        let cell: &mut MinesweeperCell = self.cell_at(coord).expect("cell coord is not in dim when generating cell coords");
+                        cell.set_coord(coord);
+                    }
+                }
+            }
+        }
+        self.set_active_cell(true);
+    }
+
+    fn set_active_cell(&mut self, t: bool) {
+        self.cell_at(self.loc).unwrap().set_active(t);
+        self.do_in_neighbourhood(self.loc, |c| c.set_active_neighbourhood(t));
     }
 
     fn dim_to_str(&self) -> String {
         format!("{} {} {} {}", self.dim.x, self.dim.y, self.dim.z, self.dim.w)
     }
 
-    fn cell_at(&self, p: Point) -> MinesweeperCell {
+    fn cell_at(&mut self, p: Point) -> Option<&mut MinesweeperCell> {
         if p < self.dim {
-            self.field[p.to_1d()]
+            return Some(&mut self.field[p.to_1d(self.dim)]);
+        }
+        return None;
+    }
+
+    fn do_in_neighbourhood(&mut self, p: Point, f: impl Fn(&mut MinesweeperCell)) {
+        for w in -1..=1 {
+            for z in -1..=1 {
+                for y in -1..=1 {
+                    for x in -1..=1 {
+                        let coord = p.offset(Point {x: x, y: y, z: z, w: w});
+                        if (Point {x: -1, y: -1, z: -1, w: -1}) < coord && coord < self.dim {
+                            //this comp is on purpose cause it kinda funky...
+                            f(self.cell_at(coord).unwrap());
+                        }
+                    }
+                }
+            }
         }
     }
 
-    fn place_mines(&self) {
-        for b in 0..self.mines {
-            while true {
-                coord = Point {x: 0, y: 0, z: 0, w: 0}.random_range(self.rng, self.dim);
+    fn place_mines(&mut self) {
+        for _ in 0..self.mines {
+            loop {
+                let mut coord = Point {x: 0, y: 0, z: 0, w: 0};
+                coord.random_range(&mut self.rng, self.dim);
+                let cell: &mut MinesweeperCell = self.cell_at(coord).expect("cell coord is not in dim");
+                if !cell.is_bomb {
+                    cell.set_bomb(true);
+                    self.do_in_neighbourhood(coord, |c| c.inc_all());
+                    break;
+                }
             }
+        }
+    }
+
+    fn move_right_x(&mut self) {
+        if self.loc.x+1 < self.dim.x {
+            self.set_active_cell(false);
+            self.loc.x += 1;
+            self.set_active_cell(true);
+        }
+    }
+
+    fn move_right_z(&mut self) {
+        if self.loc.z+1 < self.dim.z {
+            self.set_active_cell(false);
+            self.loc.z += 1;
+            self.set_active_cell(true);
+        }
+    }
+
+    fn move_left_x(&mut self) {
+        if self.loc.x-1 >= 0 {
+            self.set_active_cell(false);
+            self.loc.x -= 1;
+            self.set_active_cell(true);
+        }
+    }
+
+    fn move_left_z(&mut self) {
+        if self.loc.z-1 >= 0 {
+            self.set_active_cell(false);
+            self.loc.z -= 1;
+            self.set_active_cell(true);
+        }
+    }
+
+    fn move_down_y(&mut self) {
+        if self.loc.y+1 < self.dim.y {
+            self.set_active_cell(false);
+            self.loc.y += 1;
+            self.set_active_cell(true);
+        }
+    }
+
+    fn move_down_w(&mut self) {
+        if self.loc.w+1 < self.dim.w {
+            self.set_active_cell(false);
+            self.loc.w += 1;
+            self.set_active_cell(true);
+        }
+    }
+
+    fn move_up_y(&mut self) {
+        if self.loc.y-1 >= 0 {
+            self.set_active_cell(false);
+            self.loc.y -= 1;
+            self.set_active_cell(true);
+        }
+    }
+
+    fn move_up_w(&mut self) {
+        if self.loc.w-1 >= 0 {
+            self.set_active_cell(false);
+            self.loc.w -= 1;
+            self.set_active_cell(true);
         }
     }
 }
@@ -178,6 +362,7 @@ impl App {
     pub fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
         self.running = true;
         self.field.init();
+        self.field.place_mines();
         while self.running {
             terminal.draw(|frame| self.render(frame))?;
             self.handle_crossterm_events()?;
@@ -232,6 +417,14 @@ impl App {
             (_, KeyCode::Esc | KeyCode::Char('q'))
             | (KeyModifiers::CONTROL, KeyCode::Char('c') | KeyCode::Char('C')) => self.quit(),
             // Add other key handlers here.
+            (_, KeyCode::Char('a')) => self.field.move_left_z(),
+            (_, KeyCode::Char('d')) => self.field.move_right_z(),
+            (_, KeyCode::Char('w')) => self.field.move_up_w(),
+            (_, KeyCode::Char('s')) => self.field.move_down_w(),
+            (_, KeyCode::Left) => self.field.move_left_x(),
+            (_, KeyCode::Right) => self.field.move_right_x(),
+            (_, KeyCode::Up) => self.field.move_up_y(),
+            (_, KeyCode::Down) => self.field.move_down_y(),
             _ => {}
         }
     }
